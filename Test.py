c508b37b2b5ab1364d6134330dfa4cb85b7384ee
Eric Rhodes <erhodes71@gmail.com>
2018-09-05 13:19:49 -0500

    -
    -

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
 #Test.py
+
+import requests
+
+data = {'user_name':'user&001'}
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+url = "http://erhodes.oucreate.com/Cows/Test.php"
+r = requests.post(url, headers=headers, json=data)
+
+print(r)
