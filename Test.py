94237c6c7815146235b9f124539a21a00b2a9a1c
Morales <Eric_Morales@Erics-MacBook-Pro-2.local>
2018-09-05 13:34:53 -0500

    Python api vision request
    Success in loading the information of the test image provided in the
    bucket.

--- Test.py
+++ Test.py
@@ -1,56 +1,16 @@
-#!/usr/bin/env python
+import requests
 
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+url = "https://vision.googleapis.com/v1/images:annotate"
 
+querystring = {"key":"AIzaSyDFlQtTzZtthkF0EA2gNKul38MbkEpmo_s"}
 
-def run_quickstart():
-    # [START vision_quickstart]
-    import io
-    import os
+payload = "{\n  \"requests\":[\n    {\n      \"image\":{\n        \"source\":{\n          \"imageUri\":\n            \"gs://6583929/doggo.jpg\"\n        }\n      },\n      \"features\":[\n        {\n          \"type\":\"LABEL_DETECTION\",\n          \"maxResults\":10\n        }\n      ]\n    }\n  ]\n}\n\n"
+headers = {
+    'Content-Type': "application/json",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "fa0b10e3-ef21-4bba-b816-4e1431858e22"
+    }
 
-    # Imports the Google Cloud client library
-    # [START vision_python_migration_import]
-    from google.cloud import vision
-    from google.cloud.vision import types
-    # [END vision_python_migration_import]
+response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 
-    # Instantiates a client
-    # [START vision_python_migration_client]
-    client = vision.ImageAnnotatorClient()
-    # [END vision_python_migration_client]
-
-    # The name of the image file to annotate
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        'resources/wakeupcat.jpg')
-
-    # Loads the image into memory
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-
-    image = types.Image(content=content)
-
-    # Performs label detection on the image file
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-
-    print('Labels:')
-    for label in labels:
-        print(label.description)
-    # [END vision_quickstart]
-
-
-if __name__ == '__main__':
-run_quickstart()
+print(response.text)
